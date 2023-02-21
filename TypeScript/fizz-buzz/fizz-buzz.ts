5a8bade4346d1248a2653ba7123b9b2397701eab
Dalibor Novak <dalibor79@gmail.com>
2023-02-21 17:06:01 +0100

    Extract isFizz logic

--- TypeScript/fizz-buzz/fizz-buzz.ts
+++ TypeScript/fizz-buzz/fizz-buzz.ts
@@ -1,4 +1,15 @@
+const isFizz = (n: number): boolean => {
+  return n % 3 === 0
+}
+
 export const fizzBuzz = (): string[] => {
-  const array = ['1', '2', 'Fizz', ...new Array(97)]
+  const array: string[] = []
+  for (let i = 1; i <= 100; i++) {
+    if (isFizz(i)) {
+      array.push('Fizz')
+    } else {
+      array.push(String(i))
+    }
+  }
   return array
 }
