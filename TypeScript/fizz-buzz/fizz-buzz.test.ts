2f3215bdefe210ae5891d16527bf86dc646d8e86
Rabea Gleissner <rabeagleissner@gmail.com>
2023-02-21 17:27:16 +0200

    Create setup for FizzBuzz kata

---
+++ TypeScript/fizz-buzz/fizz-buzz.test.ts
@@ -0,0 +1,7 @@
+import { fizzBuzz } from './fizz-buzz'
+
+describe('Fizz Buzz', () => {
+  it('is a passing test just to make sure everything works', () => {
+    expect(fizzBuzz()).toEqual('it works!')
+  })
+})
