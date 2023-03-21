5925edd9c8075642239a54f27cd09554722e5fd7
Rabea Gleissner <rabeagleissner@gmail.com>
2023-03-21 16:03:49 +0000

    Add failing test for number 2

--- TypeScript/kata-3-roman-parrot/roman.test.ts
+++ TypeScript/kata-3-roman-parrot/roman.test.ts
@@ -1,13 +1,15 @@
-import { roman } from "./roman";
+import { roman } from './roman'
 
-describe("roman converter", () => {
-  it("converts 1 to I", () => {
-    expect(roman(1)).toEqual("I");
-  });
+describe('roman converter', () => {
+  it('converts 1 to I', () => {
+    expect(roman(1)).toEqual('I')
+  })
 
-  it("converts 5 to V", () => {
-    expect(roman(5)).toEqual("V");
-  });
+  it('converts 2 to II', () => {
+    expect(roman(2)).toEqual('II')
+  })
 
-
-});
+  it('converts 5 to V', () => {
+    expect(roman(5)).toEqual('V')
+  })
+})
