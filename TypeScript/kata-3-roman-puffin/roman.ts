14a03c9700c070d9e6441737f56a28f725173236
Teun Mooij <tmooij@infinitaslearning.com>
2023-03-21 17:28:04 +0100

    puffin solution?

--- TypeScript/kata-3-roman-puffin/roman.ts
+++ TypeScript/kata-3-roman-puffin/roman.ts
@@ -1,15 +1,25 @@
-export const toRoman = (num: number): string => {
-  let value = num
-  let suffix = ''
-  if (String(num).slice(-1) === '5') {
-    value = value - 5
-    suffix = 'V'
-  }
-  if (value === 1000) return `M${suffix}`
-  if (value === 500) return `D${suffix}`
-  if (value === 100) return `C${suffix}`
-  if (value === 50) return `L${suffix}`
-  if (value === 10) return `X${suffix}`
-  if (num === 5) return 'V'
-  return 'I'
+const literals = [
+  { arab: 1000, roman: 'M' },
+  { arab: 900, roman: 'CM' },
+  { arab: 500, roman: 'D' },
+  { arab: 400, roman: 'CD' },
+  { arab: 100, roman: 'C' },
+  { arab: 90, roman: 'XC' },
+  { arab: 50, roman: 'L' },
+  { arab: 40, roman: 'XL' },
+  { arab: 10, roman: 'X' },
+  { arab: 9, roman: 'IX' },
+  { arab: 5, roman: 'V' },
+  { arab: 4, roman: 'IV' },
+  { arab: 1, roman: 'I' },
+];
+
+const toRomanInternal = (num: number, prefix: string = ''): string => {
+  if (num < 1) return prefix;
+
+  const literal = literals.find(literal => literal.arab <= num)!;
+  const remainder = num - literal.arab;
+  return toRomanInternal(remainder, prefix + literal.roman);
 }
+
+export const toRoman = (num: number) => toRomanInternal(num);
