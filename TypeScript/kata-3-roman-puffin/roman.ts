9929ef46d8ac178c79fa519bd90b08922d20803c
Dalibor Novak <dalibor79@gmail.com>
2023-03-21 17:09:43 +0100

    Implement solution for numbers ending in 5

--- TypeScript/kata-3-roman-puffin/roman.ts
+++ TypeScript/kata-3-roman-puffin/roman.ts
@@ -1,9 +1,15 @@
 export const toRoman = (num: number): string => {
-  if (num === 1000) return 'M'
-  if (num === 500) return 'D'
-  if (num === 100) return 'C'
-  if (num === 50) return 'L'
-  if (num === 10) return 'X'
+  let value = num
+  let suffix = ''
+  if (String(num).slice(-1) === '5') {
+    value = value - 5
+    suffix = 'V'
+  }
+  if (value === 1000) return `M${suffix}`
+  if (value === 500) return `D${suffix}`
+  if (value === 100) return `C${suffix}`
+  if (value === 50) return `L${suffix}`
+  if (value === 10) return `X${suffix}`
   if (num === 5) return 'V'
   return 'I'
 }
