a8c8f0b3c14cd0839734a42a17c4a30c410007a7
Emma Karayiannis <emma.karayiannis@gmail.com>
2023-03-21 15:57:24 +0000

    Add [15, 55, 105, 505, 1005] test

--- TypeScript/kata-3-roman-puffin/roman.test.ts
+++ TypeScript/kata-3-roman-puffin/roman.test.ts
@@ -1,15 +1,30 @@
-import { toRoman } from "./roman";
+import { toRoman } from './roman'
 
 describe('roman tests', () => {
   it('returns I for 1', () => {
     expect(toRoman(1)).toBe('I')
-  });
+  })
 
-  it.each([1, 5, 10, 50, 100, 500, 1000])('transforms the basic number %i', input => {
+  it.each([1, 5, 10, 50, 100, 500, 1000])('transforms the basic number %i', (input) => {
     const options = {
-      1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'
-    };
-
+      1: 'I',
+      5: 'V',
+      10: 'X',
+      50: 'L',
+      100: 'C',
+      500: 'D',
+      1000: 'M',
+    }
     expect(toRoman(input)).toBe(options[input as keyof typeof options])
   })
-});
+  it.each([15, 55, 105, 505, 1005])('transforms the basic number %i', (input) => {
+    const options = {
+      15: 'XV',
+      55: 'LV',
+      105: 'CV',
+      505: 'DV',
+      1005: 'MV',
+    }
+    expect(toRoman(input)).toBe(options[input as keyof typeof options])
+  })
+})
--- TypeScript/kata-3-roman-puffin/roman.ts
+++ TypeScript/kata-3-roman-puffin/roman.ts
@@ -1,3 +1,9 @@
 export const toRoman = (num: number): string => {
+  if (num === 1000) return 'M'
+  if (num === 500) return 'D'
+  if (num === 100) return 'C'
+  if (num === 50) return 'L'
+  if (num === 10) return 'X'
+  if (num === 5) return 'V'
   return 'I'
 }
