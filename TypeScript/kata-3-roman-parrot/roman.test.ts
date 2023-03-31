c0cfe0137e6ef5f5b0ce8a7cc613d1015e3e3d35
Goderik Lefebvre <lefebvregoderik@gmail.com>
2023-03-31 17:49:54 +0200

    the parrot's arabic to roman converter

--- TypeScript/kata-3-roman-parrot/roman.test.ts
+++ TypeScript/kata-3-roman-parrot/roman.test.ts
@@ -1,19 +1,41 @@
-import { roman } from './roman'
+import { roman} from './roman'
 
 describe('roman converter', () => {
-  it('converts 1 to I', () => {
+  it('the roman converter should work for units', () => {
     expect(roman(1)).toEqual('I')
-  })
-
-  it('converts 2 to II', () => {
     expect(roman(2)).toEqual('II')
-  })
-
-  it('converts 5 to V', () => {
+    expect(roman(3)).toEqual('III')
+    expect(roman(4)).toEqual('IV')
     expect(roman(5)).toEqual('V')
+    expect(roman(6)).toEqual('VI')
+    expect(roman(7)).toEqual('VII')
+    expect(roman(8)).toEqual('VIII')
+    expect(roman(9)).toEqual('IX')
+    expect(roman(0)).toEqual('')
   })
 
-  it('converts 10 to X', () => {
-    expect(roman(10)).toEqual('X')
+  it('the roman converter should use the correct symbols for tens', () => {
+    expect(roman(80)).toEqual('LXXX')
+    expect(roman(90)).toEqual('XC')
   })
+
+  it('the roman converter should use the correct symbols for hundreds', () => {
+    expect(roman(800)).toEqual('DCCC')
+    expect(roman(900)).toEqual('CM')
+  })
+
+  it('the roman converter should work correct for the thousands', () => {
+    expect(roman(3000)).toEqual('MMM')
+    expect(roman(13000)).toEqual('MMMMMMMMMMMMM')
+  })
+
+  it('the roman converter should assemble correctly the result', () => {
+    const units = Math.floor(Math.random() * 10)
+    const tens = Math.floor(Math.random() * 10)
+    const hundreds = Math.floor(Math.random() * 10)
+    const thousands = Math.floor(Math.random() * 20)
+    expect(roman(thousands*1000)+roman(hundreds*100)+roman(tens*10)+roman(units)).toEqual(roman(thousands * 1000 + hundreds * 100 + tens * 10 + units))
+  })
+
+
 })
--- TypeScript/kata-3-roman-parrot/roman.ts
+++ TypeScript/kata-3-roman-parrot/roman.ts
@@ -1,6 +1,29 @@
-export const roman = (n: number):string => {
-  if (n === 1) return 'I'
-  if (n === 2) return 'II'
-  if (n === 5) return 'V' 
-  return ''
+
+const arabicToRomanSymbols = [
+  ['X', 'V', 'I'],
+  ['C', 'L', 'X'],
+  ['M', 'D', 'C'],
+]
+
+export const roman = (n: number): string => {
+
+  let output = ''
+  splitDigits(n).forEach((v,i) => {output = convertRomanDigit(v, arabicToRomanSymbols[i]) + output})
+  if (n >= 1000) output = "M".repeat(Math.floor(n/1000)) + output
+  return output
+}
+
+const splitDigits = (n: number): number[] => {
+  return Array.from(String(n), x => (Number(x))).reverse().slice(0,3)
+}
+
+export const convertRomanDigit = (n: number, symbols:string[]): string => {
+  let output = ''
+  if (n === 9) output = symbols[0]
+  else if (n >= 4) output = symbols[1]
+
+  if (n % 5 <= 3) output = output + symbols[2].repeat(n % 5)
+  if (n % 5 === 4) output = symbols[2] + output
+
+  return output
 }
