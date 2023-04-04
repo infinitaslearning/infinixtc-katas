2d0d7130ecc3b20e716b1159286031d2755f3e2e
Dalibor Novak <dalibor79@gmail.com>
2023-04-04 16:34:26 +0200

    Kata 4 Tennis Scoring

---
+++ TypeScript/kata-4-tennis/example/tennis.test.ts
@@ -0,0 +1,7 @@
+import { example } from "./tennis";
+
+describe("Example", () => {
+  it("is a passing test just to make sure everything works", () => {
+    expect(example()).toEqual("it works!");
+  });
+});
