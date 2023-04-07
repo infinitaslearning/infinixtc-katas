0cc6e52c27bbbc65a987bb178fe8139a3ace8492
Dalibor Novak <dalibor79@gmail.com>
2023-04-07 16:17:10 +0200

    Implement with class per mode

--- TypeScript/kata-4-tennis/example/tennis.ts
+++ TypeScript/kata-4-tennis/example/tennis.ts
@@ -1,6 +1,45 @@
-type Mode = 'regular' | 'deuce' | 'game'
+interface Game {
+  readonly score: string
+  point(playerName: string): Game
+}
 
-export class TennisGame {
+class FinishedGame implements Game {
+  readonly score: string
+
+  constructor(winner: string) {
+    this.score = `game ${winner}`
+  }
+
+  point(playerName: string): Game {
+    throw new Error('Game finished')
+  }
+}
+
+class DeuceGame implements Game {
+  private leader: string | null = null
+
+  get score(): string {
+    if (this.leader === null) {
+      return 'deuce'
+    }
+    return `advantage ${this.leader}`
+  }
+
+  point(playerName: string): Game {
+    if (playerName === this.leader) {
+      return new FinishedGame(playerName)
+    }
+    if (this.leader === null) {
+      this.leader = playerName
+    } else {
+      this.leader = null
+    }
+    return this
+  }
+}
+
+class RegularGame implements Game {
+  static DISPLAY_SCORES = ['love', '15', '30', '40']
   private readonly player1Name: string
   private readonly player2Name: string
   private player1Score: number = 0
@@ -11,57 +50,50 @@ export class TennisGame {
     this.player2Name = player2Name
   }
 
-  private mode(): Mode {
-    if (this.player1Score >= 3 && this.player2Score >= 3) {
-      if (Math.abs(this.player1Score - this.player2Score) < 2) {
-        return 'deuce'
-      } else {
-        return 'game'
-      }
-    }
-
-    if (this.player1Score === 4 || this.player2Score === 4) {
-      return 'game'
-    }
-
-    return 'regular'
-  }
-
   get score(): string {
-    const displayScores = ['love', '15', '30', '40', 'game']
-    const sameScore = this.player1Score === this.player2Score
-    const leader = this.player1Score > this.player2Score ? this.player1Name : this.player2Name
-
-    // deuce section
-    if (this.mode() === 'deuce') {
-      if (sameScore) {
-        return 'deuce'
-      }
-      return `advantage ${leader}`
+    if (this.player1Score === this.player2Score) {
+      return `${RegularGame.DISPLAY_SCORES[this.player1Score]} all`
     }
-
-    // game section
-    if (this.mode() === 'game') {
-      return `game ${leader}`
-    }
-
-    // regular section
-    if (sameScore) {
-      return `${displayScores[this.player1Score]} all`
-    }
-    return `${displayScores[this.player1Score]}-${displayScores[this.player2Score]}`
+    return `${RegularGame.DISPLAY_SCORES[this.player1Score]}-${RegularGame.DISPLAY_SCORES[this.player2Score]}`
   }
 
-  point(playerName: string) {
-    if (this.mode() === 'game') {
-      throw new Error('Game finished')
-    }
+  point(playerName: string): Game {
     if (playerName === this.player1Name) {
       this.player1Score += 1
     } else if (playerName === this.player2Name) {
       this.player2Score += 1
-    } else {
+    }
+
+    if (this.player1Score === 3 && this.player2Score === 3) {
+      return new DeuceGame()
+    }
+    if (this.player1Score === 4) {
+      return new FinishedGame(this.player1Name)
+    }
+    if (this.player2Score === 4) {
+      return new FinishedGame(this.player2Name)
+    }
+    return this
+  }
+}
+
+export class TennisGame {
+  private game: Game
+  private readonly players: string[]
+
+  constructor(player1Name: string, player2Name: string) {
+    this.game = new RegularGame(player1Name, player2Name)
+    this.players = [player1Name, player2Name]
+  }
+
+  get score(): string {
+    return this.game.score
+  }
+
+  point(playerName: string) {
+    if (!this.players.includes(playerName)) {
       throw new Error('Unknown player')
     }
+    this.game = this.game.point(playerName)
   }
 }
