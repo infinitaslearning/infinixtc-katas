fad50f0e00210ef13b4d984bac4963cab06c79a2
Teun Mooij <tmooij@infinitaslearning.com>
2023-05-02 17:18:37 +0200

    five minutes row test

--- TypeScript/kata-6-berlin-clock-dover/berlin-clock.test.ts
+++ TypeScript/kata-6-berlin-clock-dover/berlin-clock.test.ts
@@ -1,17 +1,27 @@
-import { toBerlinTime } from './berlin-clock';
+import { toFiveMinutesRow, toSingleMinutesRow } from './berlin-clock';
 
 describe('ToBerlinTime tests', () => {
   describe('single minute row', () => {
-    it('toBerlinTime returns OOOO when time is 00:00:00', () => {
-      expect((toBerlinTime('00:00:00'))).toEqual('OOOO');
+    it('toSingleMinutesRow returns OOOO when time is 00:00:00', () => {
+      expect((toSingleMinutesRow('00:00:00'))).toEqual('OOOO');
     });
 
-    it('toBerlinTime returns YYYY when time is 23:59:59', () => {
-      expect((toBerlinTime('23:59:59'))).toEqual('YYYY');
+    it('toSingleMinutesRow returns YYYY when time is 23:59:59', () => {
+      expect((toSingleMinutesRow('23:59:59'))).toEqual('YYYY');
     });
 
-    it('toBerlinTime returns YYOO when time is 12:32:00', () => {
-      expect((toBerlinTime('12:32:00'))).toEqual('YYOO');
-    });    
+    it('toSingleMinutesRow returns YYOO when time is 12:32:00', () => {
+      expect((toSingleMinutesRow('12:32:00'))).toEqual('YYOO');
+    });
+  })
+
+  describe('five minutes row', () => {
+    it('returns OOOOOOOOOOO when time is 00:00:00', () => {
+      const time = '00:00:00';
+
+      const row = toFiveMinutesRow(time);
+
+      expect(row).toEqual('OOOOOOOOOOO');
+    })
   })
 });
