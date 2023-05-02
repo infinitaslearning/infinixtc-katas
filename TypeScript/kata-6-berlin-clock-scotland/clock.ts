7e029492053f5421e5a14a77890c9060ba98dc02
Dalibor Novak <dalibor79@gmail.com>
2023-05-02 17:24:31 +0200

    Implement the single minutes row

--- TypeScript/kata-6-berlin-clock-scotland/clock.ts
+++ TypeScript/kata-6-berlin-clock-scotland/clock.ts
@@ -1,5 +1,11 @@
-type TimeConversion = (time: string) => string;
+type TimeConversion = (time: string) => string
 
-export const toBerlinTime: TimeConversion = (time:string) => {
-    return "";
+export const toBerlinTime: TimeConversion = (time: string): string => {
+  const minutes = Number(time.substring(3, 5))
+  return singleMinutesRow(minutes).padStart(24, 'O')
+}
+
+function singleMinutesRow(minutes: number): string {
+  const singleMinutes = minutes % 5
+  return 'Y'.repeat(singleMinutes).padEnd(4, 'O')
 }
