86c872ea5ddd4da7678caea43b2bc31fd9462aba
Teun Mooij <tmooij@infinitaslearning.com>
2023-05-02 17:18:37 +0200

    five minutes row test

--- TypeScript/kata-6-berlin-clock-dover/berlin-clock.ts
+++ TypeScript/kata-6-berlin-clock-dover/berlin-clock.ts
@@ -1,6 +1,10 @@
-export const toBerlinTime = (digitaltime: string): string => {
-  const minutes = digitaltime.substring(3,5)
-  const numberOfSingleMinuteLights =  parseInt(minutes) % 5
+export const toSingleMinutesRow = (digitaltime: string): string => {
+  const minutes = digitaltime.substring(3, 5)
+  const numberOfSingleMinuteLights = parseInt(minutes) % 5
   let singleMinutesRow = 'Y'.repeat(numberOfSingleMinuteLights)
   return singleMinutesRow.padEnd(4, 'O')
 }
+
+export const toFiveMinutesRow = (digitaltime: string): string => {
+  return '';
+};
