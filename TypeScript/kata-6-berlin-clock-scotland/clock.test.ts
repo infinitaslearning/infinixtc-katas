489a00a8c6a56a76bcffb6ca4d118a943e0919bf
Dalibor Novak <dalibor79@gmail.com>
2023-05-02 18:00:00 +0200

    Implement five minutes row

--- TypeScript/kata-6-berlin-clock-scotland/clock.test.ts
+++ TypeScript/kata-6-berlin-clock-scotland/clock.test.ts
@@ -1,9 +1,5 @@
 import { toBerlinTime } from './clock'
 
-function singleMinutesRow(time: string): string {
-  return time.substring(20, 24)
-}
-
 describe('Berlin Clock', () => {
   it.each`
     digitalTime   | expected
@@ -14,7 +10,22 @@ describe('Berlin Clock', () => {
     ${'12:35:00'} | ${'OOOO'}
   `('renders $expected for single minutes row at $digitalTime', ({ digitalTime, expected }) => {
     const berlinTime = toBerlinTime(digitalTime)
+    const singleMinutesRow = berlinTime.substring(20, 24)
 
-    expect(singleMinutesRow(berlinTime)).toBe(expected)
+    expect(singleMinutesRow).toBe(expected)
+  })
+
+  it.each`
+    digitalTime   | expected
+    ${'00:00:00'} | ${'OOOOOOOOOOO'}
+    ${'23:59:59'} | ${'YYRYYRYYRYY'}
+    ${'12:04:00'} | ${'OOOOOOOOOOO'}
+    ${'12:23:00'} | ${'YYRYOOOOOOO'}
+    ${'12:35:00'} | ${'YYRYYRYOOOO'}
+  `('renders $expected for five minutes row at $digitalTime', ({ digitalTime, expected }) => {
+    const berlinTime = toBerlinTime(digitalTime)
+    const fiveMinutesRow = berlinTime.substring(9, 20)
+
+    expect(fiveMinutesRow).toBe(expected)
   })
 })
--- TypeScript/kata-6-berlin-clock-scotland/clock.ts
+++ TypeScript/kata-6-berlin-clock-scotland/clock.ts
@@ -2,10 +2,16 @@ type TimeConversion = (time: string) => string
 
 export const toBerlinTime: TimeConversion = (time: string): string => {
   const minutes = Number(time.substring(3, 5))
-  return singleMinutesRow(minutes).padStart(24, 'O')
+  return `${fiveMinutesRow(minutes)}${singleMinutesRow(minutes)}`.padStart(24, 'O')
 }
 
 function singleMinutesRow(minutes: number): string {
   const singleMinutes = minutes % 5
   return 'Y'.repeat(singleMinutes).padEnd(4, 'O')
+}
+
+function fiveMinutesRow(minutes: number): string {
+  const fullRow = 'YYRYYRYYRYY'
+  const fiveMinutes = Math.floor(minutes / 5)
+  return fullRow.substring(0, fiveMinutes).padEnd(11, 'O')
 }
