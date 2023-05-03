175529849841d15c819b96103611b613c3377215
Dalibor Novak <dalibor79@gmail.com>
2023-05-03 10:33:48 +0200

    Revert "Team scotland FTW"
    This reverts commit a682b85ff1060e7ee84a6db1e0f0f28bda43f48c.

--- TypeScript/kata-6-berlin-clock-scotland/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { example } from "./example";
-
-describe("Example", () => {
-  it("is a passing test just to make sure everything works", () => {
-    expect(example()).toEqual("it works!");
-  });
-});
--- TypeScript/kata-6-berlin-clock-scotland/example.ts
+++
@@ -1,3 +0,0 @@
-export const example = () => {
-  return 'it works!'
-}
