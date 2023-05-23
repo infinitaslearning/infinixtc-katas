2a45228be613e0cfc832e1cb8184c5a02b53ca96
Dalibor Novak <dalibor79@gmail.com>
2023-05-23 16:48:08 +0200

    Pass a day trip test

--- TypeScript/kata-7-travelling-johnny/travelling-johnny.test.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.test.ts
@@ -2,6 +2,10 @@ import schengenTime from './travelling-johnny'
 
 describe('Travelling Johnny', () => {
   it('returns 90 when no trips booked', () => {
-    expect(schengenTime([])).toBe(90)
+    expect(schengenTime([], new Date())).toBe(90)
+  })
+
+  it('returns 89 when a day trip is booked', () => {
+    expect(schengenTime([[new Date('2023-05-01'), new Date('2023-05-01')]], new Date('2023-05-10'))).toBe(89)
   })
 })
--- TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
@@ -1,3 +1,14 @@
-const schengenTime = (dates: []): number => 90
+const calculateDaysInTrip = (startDate: Date, endDate: Date): number => {
+  const oneDay = 1000 * 60 * 60 * 24
+  const diff = endDate.getTime() - startDate.getTime()
+  return Math.round(diff / oneDay) + 1
+}
+
+const schengenTime = (dates: [Date, Date][], checkDate: Date): number => {
+  if (dates.length == 1) {
+    return 90 - calculateDaysInTrip(dates[0][0], dates[0][1])
+  }
+  return 90
+}
 
 export default schengenTime
