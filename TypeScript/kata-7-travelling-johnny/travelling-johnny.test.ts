55c0624e58c9fa014e16fb7f68c9b4eed62074bd
Dalibor Novak <dalibor79@gmail.com>
2023-05-23 17:15:27 +0200

    travelling johnny with trip outside window

--- TypeScript/kata-7-travelling-johnny/travelling-johnny.test.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.test.ts
@@ -8,4 +8,8 @@ describe('Travelling Johnny', () => {
   it('returns 89 when a day trip is booked', () => {
     expect(schengenTime([[new Date('2023-05-01'), new Date('2023-05-01')]], new Date('2023-05-10'))).toBe(89)
   })
+
+  it('returns 90 when a day trip is booked but outside of the 180 window', () => {
+    expect(schengenTime([[new Date('2022-05-01'), new Date('2022-05-01')]], new Date('2023-05-10'))).toBe(90)
+  })
 })
--- TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
@@ -1,14 +1,22 @@
 type Trip = [Date, Date]
 
-const calculateDaysInTrip = ([startDate, endDate]: Trip): number => {
+const daysBetween = (startDate: Date, endDate: Date): number => {
   const oneDay = 1000 * 60 * 60 * 24
   const diff = endDate.getTime() - startDate.getTime()
-  return Math.round(diff / oneDay) + 1
+  return Math.round(diff / oneDay)
 }
 
+const calculateDaysInTrip = ([startDate, endDate]: Trip): number => {
+  return daysBetween(startDate, endDate) + 1
+}
+
+const isRelevantTrip = ([, endDate]: Trip, checkDate: Date): boolean => daysBetween(endDate, checkDate) < 180
+
 const schengenTime = (trips: Trip[], checkDate: Date): number => {
-  if (trips.length == 1) {
-    return 90 - calculateDaysInTrip(trips[0])
+  const relevantTrips = trips.filter((trip) => isRelevantTrip(trip, checkDate))
+
+  if (relevantTrips.length == 1) {
+    return 90 - calculateDaysInTrip(relevantTrips[0])
   }
   return 90
 }
