0b02a7a0559cced58d3c6d7950be2ae25bf52f30
Dalibor Novak <dalibor79@gmail.com>
2023-05-23 17:00:54 +0200

    Refactor travelling johnny

--- TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
@@ -1,12 +1,14 @@
-const calculateDaysInTrip = (startDate: Date, endDate: Date): number => {
+type Trip = [Date, Date]
+
+const calculateDaysInTrip = ([startDate, endDate]: Trip): number => {
   const oneDay = 1000 * 60 * 60 * 24
   const diff = endDate.getTime() - startDate.getTime()
   return Math.round(diff / oneDay) + 1
 }
 
-const schengenTime = (dates: [Date, Date][], checkDate: Date): number => {
-  if (dates.length == 1) {
-    return 90 - calculateDaysInTrip(dates[0][0], dates[0][1])
+const schengenTime = (trips: Trip[], checkDate: Date): number => {
+  if (trips.length == 1) {
+    return 90 - calculateDaysInTrip(trips[0])
   }
   return 90
 }
