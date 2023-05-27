6760cb140555a097934ec9df0ef899f88baea62b
Goderik Lefebvre <lefebvregoderik@gmail.com>
2023-05-27 11:22:02 +0200

    Take rolling window into account when calculating days to spend

--- TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
@@ -18,10 +18,26 @@ const tripsToRelevantDays = (trips: Trip[], checkDate: Date) => {
 }
 
 const schengenTime = (trips: Trip[], checkDate: Date): number => {
-  const daysSpendInSchengenArr: Date[] = tripsToRelevantDays(trips, checkDate)
+  let daysSpendInSchengenArr: Date[] = tripsToRelevantDays(trips, checkDate)
   const daysInSchengen = daysSpendInSchengenArr.length
 
-  return 90 - daysInSchengen
+  if (daysInSchengen > 90) return 90 - daysInSchengen
+
+  let counter: number = 0
+  let startWindow = addDays(checkDate, -179)
+  let endWindow = checkDate
+  if (!daysSpendInSchengenArr.some(d => isDateEqual(d, checkDate))) daysSpendInSchengenArr.push(checkDate)
+
+  while (daysSpendInSchengenArr.length <= 90) {
+    counter += 1
+    startWindow = addDays(startWindow, 1)
+    endWindow = addDays(endWindow, 1)
+    if (!daysSpendInSchengenArr.some(d => isDateEqual(d, endWindow))) daysSpendInSchengenArr.push(endWindow)
+    daysSpendInSchengenArr = daysSpendInSchengenArr.filter(d => isDateLessOrEqual(startWindow, d))
+  }
+
+  return counter
+
 }
 
 export default schengenTime
