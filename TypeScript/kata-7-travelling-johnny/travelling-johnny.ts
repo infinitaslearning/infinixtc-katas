8157d65673864d9c29bfcbd270d2a509638b7e11
Goderik Lefebvre <lefebvregoderik@gmail.com>
2023-05-28 10:07:21 +0200

    refactoring

--- TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
+++ TypeScript/kata-7-travelling-johnny/travelling-johnny.ts
@@ -5,35 +5,35 @@ export type Trip = [Date, Date]
 const SCHENGEN_WINDOW = 180
 const MAX_DAYS_IN_SCHENGEN = 90
 const addDateToArr = (relevantDays: Date[], dayToAdd: Date) => { if (!relevantDays.some(d => isDateEqual(d, dayToAdd))) relevantDays.push(dayToAdd)}
+const filterDatesInWindow = (dates: Date[], referenceDate: Date) => dates.filter(d => isDateLessOrEqual(addDays(referenceDate, -(SCHENGEN_WINDOW - 1)), d))
 
-const getRelevantDaysFromTrips = (trips: Trip[], checkDate: Date) => {
+const getDatesFromTrip = (trips: Trip[]) => {
   let result: Date[] = []
-  let startWindow: Date = addDays(checkDate, - (SCHENGEN_WINDOW-1))
-
-  trips.forEach(([startDate, endDate]: Trip) => {
-    let pointer: Date = new Date(startDate)
-    while (isDateLessOrEqual(pointer,endDate)) {
-      if (isDateLessOrEqual(startWindow, pointer)) addDateToArr(result, pointer)
-      pointer = addDays(pointer, 1)
+  trips.forEach(([startDateTrip, endDateTrip]: Trip) => {
+    let datePointer: Date = new Date(startDateTrip)
+    while (isDateLessOrEqual(datePointer,endDateTrip)) {
+      addDateToArr(result, datePointer)
+      datePointer = addDays(datePointer, 1)
     }
   })
   return result;
 }
 
 const schengenTime = (trips: Trip[], checkDate: Date): number => {
-  let schengenDaysInWindow: Date[] = getRelevantDaysFromTrips(trips, checkDate)
+  let tripDates = getDatesFromTrip(trips)
+  let relevantDates: Date[] = filterDatesInWindow(tripDates, checkDate)
 
-  if (schengenDaysInWindow.length > MAX_DAYS_IN_SCHENGEN) return MAX_DAYS_IN_SCHENGEN - schengenDaysInWindow.length
+  if (relevantDates.length > MAX_DAYS_IN_SCHENGEN) return MAX_DAYS_IN_SCHENGEN - relevantDates.length
 
   let counter = 0
   let datePointer = checkDate
-  addDateToArr(schengenDaysInWindow, datePointer)
+  addDateToArr(relevantDates, datePointer)
 
-  while (schengenDaysInWindow.length <= MAX_DAYS_IN_SCHENGEN) {
+  while (relevantDates.length <= MAX_DAYS_IN_SCHENGEN) {
     counter += 1
     datePointer = addDays(datePointer, 1)
-    addDateToArr(schengenDaysInWindow, datePointer)
-    schengenDaysInWindow = schengenDaysInWindow.filter(d => isDateLessOrEqual(addDays(datePointer, -(SCHENGEN_WINDOW - 1)), d))
+    addDateToArr(relevantDates, datePointer)
+    relevantDates = filterDatesInWindow(relevantDates, datePointer)
   }
 
   return counter
