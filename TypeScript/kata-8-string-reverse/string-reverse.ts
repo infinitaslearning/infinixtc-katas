9d3abb73fb4f57cb6e41fefdf05aa03aa6595dd1
Dalibor Novak <dalibor79@gmail.com>
2023-06-06 16:31:42 +0200

    Implementation with reduce

--- TypeScript/kata-8-string-reverse/string-reverse.ts
+++ TypeScript/kata-8-string-reverse/string-reverse.ts
@@ -1,3 +1,6 @@
 const first = (input: string) => input.split('').reverse().join('')
+const second = (input: string) => input.split('').reduce((acc, currentCharacter) => {
+  return [currentCharacter, ...acc]
+}, [] as string[]).join('')
 
-export const implementations: ((input: string) => string)[] = [first]
+export const implementations: ((input: string) => string)[] = [first, second]
