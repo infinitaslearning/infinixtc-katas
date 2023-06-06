4616c350cb7a36032be57e2426e113de3e2ff792
Dalibor Novak <dalibor79@gmail.com>
2023-06-06 16:47:09 +0200

    String > array

--- TypeScript/kata-8-string-reverse/string-reverse.ts
+++ TypeScript/kata-8-string-reverse/string-reverse.ts
@@ -1,17 +1,40 @@
 const first = (input: string) => input.split('').reverse().join('')
-const second = (input: string) => input.split('').reduce((acc, currentCharacter) => {
-  return [currentCharacter, ...acc]
-}, [] as string[]).join('')
+
+const second = (input: string) =>
+  input
+    .split('')
+    .reduce((acc, currentCharacter) => {
+      return [currentCharacter, ...acc]
+    }, [] as string[])
+    .join('')
+
 const third = (input: string) => {
-    const inputLength = input.length
-    const result = []
+  const inputLength = input.length
+  const result = []
 
-    for(let index = inputLength -1 ; index >= 0; index--){
-        const currentChar = input.charAt(index)
-        result.push(currentChar)
-    }
+  for (let index = inputLength - 1; index >= 0; index--) {
+    const currentChar = input.charAt(index)
+    result.push(currentChar)
+  }
 
-    return result.join('')
+  return result.join('')
 }
 
-export const implementations: ((input: string) => string)[] = [first, second, third]
+const fourth = (input: string) =>
+  input.split('').reduce((acc, currentCharacter) => {
+    return currentCharacter + acc
+  }, '')
+
+const fifth = (input: string) => {
+  const inputLength = input.length
+  let result = ''
+
+  for (let index = inputLength - 1; index >= 0; index--) {
+    const currentChar = input.charAt(index)
+    result += currentChar
+  }
+
+  return result
+}
+
+export const implementations: ((input: string) => string)[] = [first, second, third, fourth, fifth]
