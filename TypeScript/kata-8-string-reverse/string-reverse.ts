5b51967c11e365c4c1b1f5b03bff5bf4efe237d9
Dalibor Novak <dalibor79@gmail.com>
2023-06-06 16:43:37 +0200

    Implementation using for loop

--- TypeScript/kata-8-string-reverse/string-reverse.ts
+++ TypeScript/kata-8-string-reverse/string-reverse.ts
@@ -2,5 +2,16 @@ const first = (input: string) => input.split('').reverse().join('')
 const second = (input: string) => input.split('').reduce((acc, currentCharacter) => {
   return [currentCharacter, ...acc]
 }, [] as string[]).join('')
+const third = (input: string) => {
+    const inputLength = input.length
+    const result = []
 
-export const implementations: ((input: string) => string)[] = [first, second]
+    for(let index = inputLength -1 ; index >= 0; index--){
+        const currentChar = input.charAt(index)
+        result.push(currentChar)
+    }
+
+    return result.join('')
+}
+
+export const implementations: ((input: string) => string)[] = [first, second, third]
