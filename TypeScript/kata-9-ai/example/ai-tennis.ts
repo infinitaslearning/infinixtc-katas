b08804fc6b331424ed3fe855a38f09c12b64955f
Rabea Gleissner <rabeagleissner@gmail.com>
2023-11-07 17:31:20 +0200

    4th result after pasting in the test error

--- TypeScript/kata-9-ai/example/ai-tennis.ts
+++ TypeScript/kata-9-ai/example/ai-tennis.ts
@@ -3,6 +3,7 @@ class TennisGame {
   private player2Name: string;
   private player1Points: number;
   private player2Points: number;
+  private gameFinished: boolean;
 
   constructor(player1Name: string, player2Name: string) {
     if (player1Name === player2Name) {
@@ -12,9 +13,14 @@ class TennisGame {
     this.player2Name = player2Name;
     this.player1Points = 0;
     this.player2Points = 0;
+    this.gameFinished = false;
   }
 
   point(playerName: string): void {
+    if (this.gameFinished) {
+      throw new Error('Game finished');
+    }
+
     if (playerName === this.player1Name) {
       this.player1Points++;
     } else if (playerName === this.player2Name) {
@@ -22,26 +28,28 @@ class TennisGame {
     } else {
       throw new Error('Unknown player');
     }
+
+    this.checkGameFinished();
+  }
+
+  private checkGameFinished(): void {
+    if (this.player1Points >= 4 && this.player1Points - this.player2Points >= 2) {
+      this.gameFinished = true;
+    } else if (this.player2Points >= 4 && this.player2Points - this.player1Points >= 2) {
+      this.gameFinished = true;
+    }
   }
 
   get score(): string {
+    if (this.gameFinished) {
+      return `game ${this.player1Points > this.player2Points ? this.player1Name : this.player2Name}`;
+    }
+
     if (this.player1Points >= 3 && this.player2Points >= 3) {
       if (this.player1Points === this.player2Points) {
         return 'deuce';
       } else if (Math.abs(this.player1Points - this.player2Points) === 1) {
         return `advantage ${this.player1Points > this.player2Points ? this.player1Name : this.player2Name}`;
-      }
-    }
-
-    if (this.player1Points >= 4) {
-      if (this.player1Points - this.player2Points >= 2) {
-        return `game ${this.player1Name}`;
-      }
-    }
-
-    if (this.player2Points >= 4) {
-      if (this.player2Points - this.player1Points >= 2) {
-        return `game ${this.player2Name}`;
       }
     }
 
