176b6dcf0d38b5e2ba74c2ed8a3776f25a59bbc9
Rabea Gleissner <rabeagleissner@gmail.com>
2023-11-07 17:50:41 +0200

    AI generate test - second try

--- TypeScript/kata-9-ai/example/generated-ai-tennis.test.ts
+++ TypeScript/kata-9-ai/example/generated-ai-tennis.test.ts
@@ -1,77 +1,75 @@
-import { vit, assert } from 'vitest';
-import { TennisGame, RegularGame, DeuceGame, FinishedGame } from './TennisGame'; // Make sure to adjust the import path as needed
+import { test } from 'vitest'
+import { TennisGame, RegularGame, DeuceGame, FinishedGame } from './TennisGame' // Adjust the import path as needed
 
-vit('TennisGame', () => {
-  let tennisGame: TennisGame;
+test('TennisGame', () => {
+  let tennisGame: TennisGame
 
-  vit.beforeEach(() => {
-    tennisGame = new TennisGame('Player1', 'Player2');
-  });
+  test.beforeEach(() => {
+    tennisGame = new TennisGame('Player1', 'Player2')
+  })
 
-  vit('initial score should be "love all"', () => {
-    assert.is(tennisGame.score, 'love all');
-  });
+  test('initial score should be "love all"', () => {
+    assert.is(tennisGame.score, 'love all')
+  })
 
-  vit('point method should update the score correctly', () => {
-    tennisGame.point('Player1');
-    assert.is(tennisGame.score, '15 love');
+  test('point method should update the score correctly', () => {
+    tennisGame.point('Player1')
+    assert.is(tennisGame.score, '15 love')
 
-    tennisGame.point('Player2');
-    assert.is(tennisGame.score, '15 all');
+    tennisGame.point('Player2')
+    assert.is(tennisGame.score, '15 all')
 
-    tennisGame.point('Player1');
-    tennisGame.point('Player1');
-    assert.is(tennisGame.score, '40 15');
+    tennisGame.point('Player1')
+    tennisGame.point('Player1')
+    assert.is(tennisGame.score, '40 15')
 
-    tennisGame.point('Player2');
-    tennisGame.point('Player2');
-    assert.is(tennisGame.score, 'deuce');
-  });
+    tennisGame.point('Player2')
+    tennisGame.point('Player2')
+    assert.is(tennisGame.score, 'deuce')
+  })
 
-  vit('should handle game ending correctly', () => {
-    tennisGame.point('Player1');
-    tennisGame.point('Player1');
-    tennisGame.point('Player1');
-    assert.is(tennisGame.score, 'game Player1');
-  });
-});
+  test('should handle game ending correctly', () => {
+    tennisGame.point('Player1')
+    tennisGame.point('Player1')
+    tennisGame.point('Player1')
+    assert.is(tennisGame.score, 'game Player1')
+  })
+})
 
-vit('DeuceGame', () => {
-  let deuceGame: DeuceGame;
+test('DeuceGame', () => {
+  let deuceGame: DeuceGame
 
-  vit.beforeEach(() => {
-    deuceGame = new DeuceGame();
-  });
+  test.beforeEach(() => {
+    deuceGame = new DeuceGame()
+  })
 
-  vit('initial score should be "deuce"', () => {
-    assert.is(deuceGame.score, 'deuce');
-  });
+  test('initial score should be "deuce"', () => {
+    assert.is(deuceGame.score, 'deuce')
+  })
 
-  vit('point method should update the score correctly', () => {
-    deuceGame.point('Player1');
-    assert.is(deuceGame.score, 'advantage Player1');
+  test('point method should update the score correctly', () => {
+    deuceGame.point('Player1')
+    assert.is(deuceGame.score, 'advantage Player1')
 
-    deuceGame.point('Player2');
-    assert.is(deuceGame.score, 'deuce');
-  });
-});
+    deuceGame.point('Player2')
+    assert.is(deuceGame.score, 'deuce')
+  })
+})
 
-vit('FinishedGame', () => {
-  let finishedGame: FinishedGame;
+test('FinishedGame', () => {
+  let finishedGame: FinishedGame
 
-  vit.beforeEach(() => {
-    finishedGame = new FinishedGame('Player1');
-  });
+  test.beforeEach(() => {
+    finishedGame = new FinishedGame('Player1')
+  })
 
-  vit('score should be "game Player1"', () => {
-    assert.is(finishedGame.score, 'game Player1');
-  });
+  test('score should be "game Player1"', () => {
+    assert.is(finishedGame.score, 'game Player1')
+  })
 
-  vit('point method should throw an error', () => {
+  test('point method should throw an error', () => {
     assert.throws(() => {
-      finishedGame.point('Player1');
-    }, 'Game finished');
-  });
-});
-
-vit.run();
+      finishedGame.point('Player1')
+    }, 'Game finished')
+  })
+})
